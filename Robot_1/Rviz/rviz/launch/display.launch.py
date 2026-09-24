f60bba8fe0e470e8e57506e4f539cb8510a25f0f
marivrodriguez <marivrodriguez125@gmail.com>
2026-09-23 19:17:19 -0500

    Add files via upload

---
+++ Robot_1/Rviz/rviz/launch/display.launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+
+    package_path = get_package_share_directory('rviz')
+
+    urdf_file = os.path.join(
+        package_path,
+        'urdf',
+        'rviz.urdf'
+    )
+
+    with open(urdf_file, 'r') as file:
+        robot_description = file.read()
+
+    return LaunchDescription([
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[
+                {'robot_description': robot_description}
+            ]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2'
+        )
+    ])
